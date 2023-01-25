0dde2586995f1d234a09165a23750df6d592be82
randymz10 <randymz10@gmail.com>
2023-01-25 20:03:18 -0300

    Add new features into the project

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -4,17 +4,43 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
 
 @Controller
 public class FormController {
-    
+
     @GetMapping("/form")
     public String form(Model model) {
+        model.addAttribute("titulo", "Formulario usuarios");
         return "form";
     }
 
+    // @PostMapping("/form")
+    // public String procesar(Model model,
+    //         @RequestParam String username,
+    //         @RequestParam String password,
+    //         @RequestParam String email) {
+
+    //     Usuario usuario = new Usuario();
+
+    //     usuario.setUsername(username);
+    //     usuario.setPassword(password);
+    //     usuario.setEmail(email);
+
+    //     model.addAttribute("titulo", "Resultado form");
+    //     model.addAttribute("usuario", usuario);
+
+    //     return "resultado";
+    // }
+
     @PostMapping("/form")
-    public String procesar(Model model) {
-        return "resultados";
+    public String procesar(Usuario usuario, Model model) {
+
+        model.addAttribute("titulo", "Resultado form");
+        model.addAttribute("usuario", usuario);
+
+        return "resultado";
     }
 }
