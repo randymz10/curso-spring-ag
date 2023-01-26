a0e8d513f628fe200b332deb288479644573bfe8
randymz10 <randymz10@gmail.com>
2023-01-25 23:57:34 -0300

    Add new features

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -48,12 +48,16 @@ public class FormController {
         
         model.addAttribute("titulo", "Resultado form");
 
+        // if(result.hasErrors()) {
+        //     Map<String, String> errores = new HashMap<>();
+        //     result.getFieldErrors().forEach(err -> {
+        //         errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+        //     });
+        //     model.addAttribute("error", errores);
+        //     return "form";
+        // }
+
         if(result.hasErrors()) {
-            Map<String, String> errores = new HashMap<>();
-            result.getFieldErrors().forEach(err -> {
-                errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
-            });
-            model.addAttribute("error", errores);
             return "form";
         }
 
